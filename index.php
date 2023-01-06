641de895d7cc353ae3a98870d3ad780b1a0c355f
Scripted <cb443151@gmail.com>
2023-01-06 17:55:52 +0000

    basic stuff

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="style.css"> 
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test</title>  
+    <link rel="icon" href="">
+
+</head>
+<body> 
+    
+    <form>
+        <input type="text"> 
+    </form>
+
+
+</body>
+
+
+
+
+
+</html> 
